2fe96befbe53c768f7ebf17e5499aca386f980fa
Shivam Agrawal <agrawalshivam66@gmail.com>
2018-10-31 20:19:04 +0530

    update

--- Control flow graph/cfg.java
+++ Control flow graph/cfg.java
@@ -10,15 +10,34 @@ public class cfg {
 
     public static void funName() throws Exception{
         int count = 0;
-	
+        String lines[] = new String[100];
+
+	// parsing file to find the number of lines and storing lines
   Scanner sc = new Scanner(new File("cfile.cpp"));
     while(sc.hasNext()){
         count++;
         String s = sc.nextLine();
-        System.out.print(count);
-        System.out.println(" "+s);
+        lines[count]=s.trim();
     }
-sc.close();
+    sc.close();
+
+       for(int i=1;i<=count;i++){
+        System.out.println(lines[i]);
+    }
+
+    // initializing and declaring the matrix to 0
+    int m[][] = new int[count][count];
+
+    for(int i=0;i<count;i++){
+        for (int j=1;j<count;j++)
+        {
+            m[i][j]=0;
+        }
+    }
+      
+
+
+
 
         }
     }
